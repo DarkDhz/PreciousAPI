e5685b52cb1150cb34db6abc56e6041f07b7eed9
VirtualPlanetNT <wDN%JD!:$$*+A6eZ4E}u-Sr>
2020-04-07 14:58:00 +0200

    Blocked ps allow

--- src/main/java/dev/darkhorizon/es/psAPI/events/PSEvents.java
+++ src/main/java/dev/darkhorizon/es/psAPI/events/PSEvents.java
@@ -3,6 +3,7 @@ package dev.darkhorizon.es.psAPI.events;
 import com.sun.org.apache.bcel.internal.generic.RETURN;
 import dev.darkhorizon.es.psAPI.Main;
 import net.sacredlabyrinth.Phaed.PreciousStones.PreciousStones;
+import net.sacredlabyrinth.Phaed.PreciousStones.field.Field;
 import net.sacredlabyrinth.Phaed.PreciousStones.field.FieldFlag;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
@@ -10,11 +11,26 @@ import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.Action;
 import org.bukkit.event.block.BlockPlaceEvent;
+import org.bukkit.event.player.PlayerCommandPreprocessEvent;
 import org.bukkit.event.player.PlayerInteractEvent;
 
 public class PSEvents implements Listener {
 
     private final Main plugin = Main.getPlugin(Main.class);
+
+
+    @EventHandler(priority = EventPriority.HIGHEST)
+    public void blockCommands(PlayerCommandPreprocessEvent e) {
+        Player p = e.getPlayer();
+        String cmd = e.getMessage();
+        if (cmd.startsWith("/ps allow")) {
+             Field launcher_mena = PreciousStones.getInstance().getForceFieldManager().getEnabledSourceField(p.getLocation(), FieldFlag.ALL);
+             if (launcher_mena != null && !launcher_mena.getOwner().equals(p.getName())) {
+                 e.setCancelled(true);
+                 p.sendMessage("¡Solo el propietario de la mena esta autorizado a usar este comando!");
+             }
+        }
+    }
 
     @EventHandler(priority = EventPriority.HIGHEST)
     public void onInteract(PlayerInteractEvent e) {
